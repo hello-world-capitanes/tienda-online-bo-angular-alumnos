46af4ac4cc9020944ee3d8d26f2bc5f73821a513
DANIEL-PARDO\Daniel Pardo <danielpardo@devanddel.com>
2022-03-16 12:58:30 +0100

    Commit componente producto

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProductComponent } from './product/product.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProductComponent
+  ],
   imports: [
     CommonModule
   ]
