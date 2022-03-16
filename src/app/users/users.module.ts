04f9abb714991ba42da10049ca633f425e22dbb7
Raúl Bravo <raulbravo@devanddel.com>
2022-03-16 13:06:38 +0100

    Añado modulo + componentes

---
+++ src/app/users/users.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormUserComponent } from './form-user/form-user.component';
+import { ListUserComponent } from './list-user/list-user.component';
+
+
+
+@NgModule({
+  declarations: [
+    FormUserComponent,
+    ListUserComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class UsersModule { }
