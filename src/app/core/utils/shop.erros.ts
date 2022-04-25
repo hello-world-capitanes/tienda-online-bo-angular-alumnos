b7b59bcaa4213b83b190888475eed10420279784
DANIEL-PARDO\Daniel Pardo <danielpardo@devanddel.com>
2022-04-25 11:20:53 +0200

    Errores tienda, correccion constructor tienda

---
+++ src/app/core/utils/shop.erros.ts
@@ -0,0 +1,21 @@
+export const SHOP_ERRORS = {
+  name: {
+    notProvided: 'No ha introducido el nombre de la tienda'
+  },
+  country: {
+    notProvided: 'No ha introducido el país'
+  },
+  province: {
+    notProvided: 'No ha introducido una provincia'
+  },
+  location: {
+    notProvided: 'No ha introducido localidad'
+  },
+  street: {
+    notProvided: 'No ha introducido la localidad'
+  },
+  cp: {
+    notProvided: 'No ha introducido código postal',
+    invalid: 'Código postal inválido'
+  }
+}
