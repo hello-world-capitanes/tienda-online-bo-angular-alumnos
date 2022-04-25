77b499e9553a885294a0fad03633e2287634a01c
Raúl Bravo <raulbravo@devanddel.com>
2022-04-25 14:09:26 +0200

    Pruebas unitarias

--- src/app/features/category/components/categories/categories.component.spec.ts
+++ src/app/features/category/components/categories/categories.component.spec.ts
@@ -19,7 +19,5 @@ describe('CategoriesComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+
 });
--- src/app/features/user/components/form-user/form-user.component.spec.ts
+++ src/app/features/user/components/form-user/form-user.component.spec.ts
@@ -19,7 +19,5 @@ describe('FormUserComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+
 });
