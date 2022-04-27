95ae43ab112f885932e87e7221204aa589bc5525
Mónica Rodríguez Pérez <monicarodriguez@devanddel.com>
2022-04-27 12:00:23 +0200

    merge con alta categoria

--- src/app/features/shop/components/shops-list/shops-list.component.ts
+++ src/app/features/shop/components/shops-list/shops-list.component.ts
@@ -1,6 +1,6 @@
-import { ShopService } from './../../shop.service';
 import { Component, OnInit } from '@angular/core';
 import { Shop } from '../../models/shop.model';
+import { ShopService } from '../../shop.service';
 
 export interface shopElement{
   name: string;
