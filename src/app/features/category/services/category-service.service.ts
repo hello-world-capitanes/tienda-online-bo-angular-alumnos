ccf93dbbb94b2c1ca52caae509605b0917fbec2d
Raúl Bravo <raulbravo@devanddel.com>
2022-05-09 12:47:03 +0200

    si existe no añadir categoria

--- src/app/features/category/services/category-service.service.ts
+++ src/app/features/category/services/category-service.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/compat/firestore';
-import { map, Observable } from 'rxjs';
+import { AngularFirestore, docChanges } from '@angular/fire/compat/firestore';
+import { isReactNative } from '@firebase/util';
+import { catchError, map, Observable, Subscription } from 'rxjs';
 import { FirestoreService } from 'src/app/core/services/firestore.service';
 import { Category } from '../models/category.model';
 
@@ -31,22 +32,32 @@ export class CategoryService extends FirestoreService{
    * @param category
    * @returns category with DB id
    */
-   async addCategory(category: Category): Promise<Category> {
+    async addCategory(category: Category): Promise<Category | undefined> {
 
-    if(await this.categoryExists(category)){
-      category.id = this.firestore.createId();
-      return this.getCollection().doc(category.id).set(category).then(() => {
-        return category;
-      });
-    }
+      const result =await this.categoryExists(category)
 
-    return category;
+      if(result===undefined){
+        category.id = this.firestore.createId();
+
+        let categoryDB = {
+          id: category.id,
+          name: category.name,
+          description: category.description,
+          active: category.active,
+        };
+
+        return this.getCollection().doc(category.id).set(Object.assign({}, categoryDB)).then(() => {
+          return categoryDB as Category;
+        })
+      }
+
+      return;
 
   }
 
-  async categoryExists(category: Category){
-    let categoryExist = (await this.getCollection().ref.doc(category.id).get()).exists;
-    return categoryExist;
+  async categoryExists(category: Category): Promise<Category | undefined> {
+    const snapshot = await this.getCollection().ref.where("name", "==", category.name).get();
+    return snapshot?.docs && snapshot.docs.length > 0 ? snapshot?.docs[0].data() as Category : undefined;
   }
 
   deleteCategory(category:Category){
@@ -67,47 +78,9 @@ export class CategoryService extends FirestoreService{
 
   }*/
 
-  addCategory2(category: Category) {
-    if (this._categoryList?.some((element) => element.name === category.name) && !category.active) {
-      return;
-    } else if (this._categoryList?.some((element) => element.name === category.name) && category.active) {
-      if (this._categoryList?.some((element) => element.name === category.name
-        && !element.active)) {
-        for (let i = 0; i < this._categoryList.length; i++) {
-          if (this._categoryList[i].name === category.name) {
-            this._categoryList[i].active;
-            this._categoryList[i].description;
-          }
-        }
-      } else if (this._categoryList?.some((element) => element.name === category.name
-        && element.active)) {
-        return;
-      }
-    } else {
-      this._categoryList?.push(category)
-    }
-
-  };
-
-  /*categoryExists(category: Category) {
-    if (this._categoryList?.some((element) => element.name === category.name)) {
-      return true;
-    } else {
-      return false;
-    };
-  }*/
-
   getAllCategories(): Category[] {
     return this._categoryList!;
   }
-
-  /*deleteCategory(value: Category) {
-    if (this._categoryList?.some(element => element.id == value.id)) {
-      this._categoryList.splice(this._categoryList.indexOf(value), 1);
-    } else {
-      return;
-    }
-  }*/
 
   public get cateogoryList(): Category[] {
     return this._categoryList!;
