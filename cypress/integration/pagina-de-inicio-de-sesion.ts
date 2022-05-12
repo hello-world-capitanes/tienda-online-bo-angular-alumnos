6ea1fd3e0ffc3944c351419bea5a3a4170dbea94
raulpradanas <raulpradanas@devanddel.com>
2022-05-12 16:58:50 +0200

    added e2e test

--- cypress/integration/pagina-de-inicio-de-sesion.ts
+++ cypress/integration/pagina-de-inicio-de-sesion.ts
@@ -21,6 +21,7 @@ describe('Inicio de sesión', function() {
     cy.get('#mat-input-0').type(usuarioCorrecto);
     cy.get('#mat-input-1').type(passwordCorrecta);
     cy.get('#loginButton').click();
+    cy.get('#sidenav').should('be.visible');
     cy.get('#sidenav');
     cy.get('#logoutButton').click();
   });
@@ -29,59 +30,59 @@ describe('Inicio de sesión', function() {
     cy.get('#mat-input-0').type('invalid@gmail.com');
     cy.get('#mat-input-1').type('12345');
     cy.get('#loginButton').click();
-    cy.get('.snackContainer');
-    cy.get('.contenedorForm');
+    cy.get('.snackContainer').should('be.visible');
+    cy.get('.contenedorForm').should('be.visible');
     cy.reload();
 
     cy.get('#mat-input-0').type(usuarioCorrecto);
     cy.get('#mat-input-1').type('fasdf32eds');
     cy.get('#loginButton').click();
-    cy.get('.snackContainer');
-    cy.get('.contenedorForm');
+    cy.get('.snackContainer').should('be.visible');
+    cy.get('.contenedorForm').should('be.visible');
     cy.reload();
 
     cy.get('#mat-input-0').type("a@afasd");
     cy.get('#mat-input-1').type(passwordCorrecta);
     cy.get('#loginButton').click();
-    cy.get('.snackContainer');
-    cy.get('.contenedorForm');
+    cy.get('.snackContainer').should('be.visible');
+    cy.get('.contenedorForm').should('be.visible');
     cy.reload();
 
     cy.get('#mat-input-0').type(" ");
     cy.get('#mat-input-1').type(" ");
     cy.get('#loginButton').click();
-    cy.get('.snackContainer');
-    cy.get('.contenedorForm');
+    cy.get('.snackContainer').should('be.visible');
+    cy.get('.contenedorForm').should('be.visible');
     cy.reload();
 
     cy.get('#loginButton').click();
-    cy.get('.snackContainer');
-    cy.get('.contenedorForm');
+    cy.get('.snackContainer').should('be.visible');
+    cy.get('.contenedorForm').should('be.visible');
   });
 
   it('Visitar páginas no accesibles loggeo previo', function() {
     cy.visit('http://localhost:4200/products');
-    cy.get('.snackContainer');
-    cy.get('.contenedorForm');
+    cy.get('.snackContainer').should('be.visible');
+    cy.get('.contenedorForm').should('be.visible');
 
     cy.visit('http://localhost:4200/shops');
-    cy.get('.snackContainer');
-    cy.get('.contenedorForm');
+    cy.get('.snackContainer').should('be.visible');
+    cy.get('.contenedorForm').should('be.visible');
 
     cy.visit('http://localhost:4200/users');
-    cy.get('.snackContainer');
-    cy.get('.contenedorForm');
+    cy.get('.snackContainer').should('be.visible');
+    cy.get('.contenedorForm').should('be.visible');
 
     cy.visit('http://localhost:4200/categories');
-    cy.get('.snackContainer');
-    cy.get('.contenedorForm');
+    cy.get('.snackContainer').should('be.visible');
+    cy.get('.contenedorForm').should('be.visible');
 
     cy.visit('http://localhost:4200/home');
-    cy.get('.snackContainer');
-    cy.get('.contenedorForm');
+    cy.get('.snackContainer').should('be.visible');
+    cy.get('.contenedorForm').should('be.visible');
 
     cy.visit('http://localhost:4200/sign-in');
-    cy.get('.contenedorForm');
+    cy.get('.contenedorForm').should('be.visible');
   });
 
 })
