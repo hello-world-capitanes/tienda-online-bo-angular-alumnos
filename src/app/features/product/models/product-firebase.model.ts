1989dadbdc88e9bb628fb9e50ae78bcfffee0b13
Mónica Rodríguez Pérez <monicarodriguez@devanddel.com>
2022-05-13 10:49:45 +0200

    crear para solucionar merge

---
+++ src/app/features/product/models/product-firebase.model.ts
@@ -0,0 +1,10 @@
+export interface ProductFirebase {
+  id: string
+  name: string
+  characteristics: string
+  price: number
+  description: string
+  image: string
+  active: boolean,
+  categories?: any[]
+}
