f143f33fd7e5bf74e5ecc26fd928852496f56a7f
Raúl Bravo <raulbravo@devanddel.com>
2022-05-17 13:57:42 +0200

    fix-Test E2E Modify Product

--- cypress/integration/modify-product.ts
+++ cypress/integration/modify-product.ts
@@ -4,12 +4,16 @@ describe('Modify product', () => {
 
   beforeEach(function () {
     cy.visit('http://localhost:4200/sign-in');
-  });
-
-  it('Modify product with correct data', () => {
     cy.get('#mat-input-0').type(usuarioCorrecto);
     cy.get('#mat-input-1').type(passwordCorrecta);
     cy.get('#loginButton').click();
+  });
+
+  afterEach(function() {
+    cy.get('#logoutButton').click();
+  });
+
+  it('Modify product with correct data', () => {
     cy.get('#Products > .mat-line').click();
     cy.get('#DonutsEditActive', {
       timeout: 5000,
@@ -19,6 +23,6 @@ describe('Modify product', () => {
       timeout: 5000,
     }).should('be.visible');
     cy.get('#price').type('40');
-    cy.get('#modifyProduct').submit();
+    cy.get('#modifyEnabled').click();
   });
 });
