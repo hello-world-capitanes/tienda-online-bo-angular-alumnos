6d8cb9e5e47d831bcb1e4dbce5b110c4f620514a
raulpradanas <raulpradanas@devanddel.com>
2022-05-17 15:09:03 +0200

    Some change in cypress test of shop-list

--- cypress/integration/product-list-shop.ts
+++ cypress/integration/product-list-shop.ts
@@ -42,19 +42,4 @@ describe('Modify stock', () => {
         cy.get('@buttonStock').click();
       });
   });
-
-  /* it('Decrease stock', () => {
-    cy.get('#mat-input-0').type(usuarioCorrecto);
-    cy.get('#mat-input-1').type(passwordCorrecta);
-    cy.get('#loginButton').click();
-    cy.get('.mat-list-item:nth-child(6) .mat-line').click();
-    cy.get('#mat-expansion-panel-header-5 > .mat-content').click();
-    cy.get('.cdk-focused').click();
-    cy.get('#mat-input-12').click();
-    cy.get('#mat-input-12').type('{backspace}');
-    cy.get('#mat-input-12').type('{backspace}');
-    cy.get('#mat-input-12').type('{backspace}');
-    cy.get('#mat-input-12').type('45');
-    cy.get('.cdk-focused').click();
-  }); */
 });
