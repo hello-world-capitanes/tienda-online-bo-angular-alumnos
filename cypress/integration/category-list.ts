0af61ff12af247a8318dfeb1fe950ec2bd180c69
Osman Mehmed <osman@devanddel.com>
2022-05-18 12:40:06 +0200

    Fixed Category.list.ts tests

--- cypress/integration/category-list.ts
+++ cypress/integration/category-list.ts
@@ -4,10 +4,17 @@ describe('Category list test section', () => {
 
   beforeEach(function () {
     cy.visit('http://localhost:4200/sign-in');
-    cy.visit('http://localhost:4200/');
+    cy.get('#emailInput').type(usuarioCorrecto);
+    cy.get('#passwordInput').type(passwordCorrecta);
+    cy.get('#loginButton').click();
   });
 
   it('Disable category and see if Product dont have it listed', () => {
+
+
+    cy.get('#Categories', {
+      timeout: 5000,
+    }).should('be.visible');
 
     cy.get('#Categories').click();
     cy.get('#LimpiezaEditNoActive .mat-icon').click();
@@ -23,7 +30,10 @@ describe('Category list test section', () => {
     cy.get('#LimpiezaEditActive .mat-icon').click();
     cy.get('#Products > .mat-icon').click();
     cy.get('#EstropajoExpansionPanel > .mat-list-item-content').click();
-    cy.get('#Limpiezachip').click();
+
+    cy.get('#Limpiezachip', {
+      timeout: 5000,
+    }).should('exist');
   });
 
   afterEach(function() {
