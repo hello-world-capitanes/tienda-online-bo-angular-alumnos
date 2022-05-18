7bc70717cc7f695f02340068927570b316b3d604
Osman Mehmed <osman@devanddel.com>
2022-05-18 12:03:00 +0200

    Fixed test category list

--- cypress/integration/category-list.ts
+++ cypress/integration/category-list.ts
@@ -4,25 +4,29 @@ describe('Category list test section', () => {
 
   beforeEach(function () {
     cy.visit('http://localhost:4200/sign-in');
+    cy.visit('http://localhost:4200/');
   });
 
   it('Disable category and see if Product dont have it listed', () => {
 
-    cy.visit('http://localhost:4200/');
     cy.get('#Categories').click();
-    cy.get('#AceiteEditNoActive .mat-icon').click();
+    cy.get('#LimpiezaEditNoActive .mat-icon').click();
     cy.get('.mat-list-item:nth-child(6) > .mat-list-item-content').click();
     cy.get('#Products').click();
-    cy.get('#PiquitosSanRoqueExpansionPanel > .mat-list-item-content').click();
+    cy.get('#EstropajoExpansionPanel > .mat-list-item-content').click();
 
-    cy.get('.mat-chip > span', {
+     cy.get('#Limpiezachip', {
       timeout: 5000,
     }).should('not.exist');
 
     cy.get('#Categories > .mat-line').click();
-    cy.get('#AceiteEditActive .mat-icon').click();
+    cy.get('#LimpiezaEditActive .mat-icon').click();
     cy.get('#Products > .mat-icon').click();
-    cy.get('#PiquitosSanRoqueExpansionPanel > .mat-list-item-content').click();
-    cy.get('.mat-chip > span').click();
+    cy.get('#EstropajoExpansionPanel > .mat-list-item-content').click();
+    cy.get('#Limpiezachip').click();
+  });
+
+  afterEach(function() {
+    cy.get('#logoutButton').click();
   });
 });
