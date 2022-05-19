e650df79f529fc0a59e6e854394cd2ea61680f19
adrianmartin <adrianmartin@devanddel.com>
2022-05-19 14:58:12 +0200

    Unit test change to work.

--- src/app/features/authentication/services/auth-api.service.spec.ts
+++ src/app/features/authentication/services/auth-api.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AuthApiService } from './auth-api.service';
 
 describe('AuthApiService', () => {
   let service: AuthApiService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+      providers: [AuthApiService]
+    });
     service = TestBed.inject(AuthApiService);
   });
 
